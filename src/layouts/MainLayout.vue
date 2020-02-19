cbc167e383fa081d5d63e70fabd08aea35fd091a
Eric Milliot <ewitz@aMacBook.local>
2020-02-19 01:38:42 -0500

    second commit to add working project

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,107 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          icon="menu"
+          aria-label="Menu"
+        />
+      </q-toolbar>
+      <div class="q-px-lg q-pt-xl q-mb-md">
+        <div class="text-h3">Todo</div>
+        <div class="text-subtitle1">{{ todaysDate }}</div>
+      </div>
+      <q-img
+        src="statics/header-bg.jpg"
+        class="header-image absolute-top" 
+        />
+    </q-header>
+
+<q-drawer
+        v-model="leftDrawerOpen"
+        show-if-above
+        :width="250"
+        :breakpoint="600"
+      >
+        <q-scroll-area style="height: calc(100% - 192px); margin-top: 192px; border-right: 1px solid #ddd">
+          <q-list padding>
+            <q-item
+              to="/"
+              exact
+              clickable
+              v-ripple>
+              <q-item-section avatar>
+                <q-icon name="list" />
+              </q-item-section>
+
+              <q-item-section>
+                Todo
+              </q-item-section>
+            </q-item>
+
+            <q-item
+              to="/help"
+              exact
+              clickable 
+              v-ripple>
+              <q-item-section avatar>
+                <q-icon name="help" />
+              </q-item-section>
+
+              <q-item-section>
+                Help
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-scroll-area>
+
+        <q-img class="absolute-top" src="statics/header-bg.jpg" style="height: 192px">
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="https://s.gravatar.com/avatar/e3a31f2c524ebd4723a15516888ae981?s=80">
+            </q-avatar>
+            <div class="text-weight-bold">Eric Milliot</div>
+            <div>@EricMilliot</div>
+          </div>
+        </q-img>
+      </q-drawer>
+
+    <q-page-container>
+      <keep-alive>
+      <router-view />
+      </keep-alive>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { date } from 'quasar'
+
+export default {
+  name: 'MainLayout',
+
+  data () {
+    return {
+      leftDrawerOpen: false,
+    }
+  },
+    computed: {
+    todaysDate() {
+      let timeStamp = Date.now()
+      return date.formatDate(timeStamp, 'dddd D MMMM')
+    }
+  }
+}
+</script>
+<style lang="scss">
+  .header-image {
+    height: 100%;
+    z-index: -1;
+    opacity: 0.7;
+    filter: grayscale(100%);
+  }
+  </style>
---
+++ src/pages/Help.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>blah blah blah blah</p>
+    <p>blah blah blah blah</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+}
+</script>
